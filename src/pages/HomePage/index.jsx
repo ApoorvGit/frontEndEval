0e776d17c2736efb2b3d5f39b4e157ee8d0bca9c
apoorvGit <apoorvmishra1000@gmail.com>
2023-03-01 18:00:07 +0530

    feat: add homepage/landing page

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import Container from '../../components/Container';
+
+export default function HomePage() {
+  return (
+    <div className="homepage">
+      <Header />
+      <Container />
+      <Footer />
+    </div>
+  );
+}
